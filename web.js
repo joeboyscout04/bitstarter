434da0fad7e37d32294cfc8c7d050d097a9a03a4
Joe Elliott <joeboyscout04@gmail.com>
2013-07-09 20:49:00 -0400

    web.js with path to index.html

--- web.js
+++ web.js
@@ -1,10 +1,10 @@
 var express = require('express');
-var fs = require('fs')
+var fs = require('fs')1
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  var buf = fs.readFileSync("index.html");
+  var buf = fs.readFileSync("$HOME/bitstarter/index.html");
   response.send(buf.toString());
 });
 
